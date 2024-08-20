f89a41581719ef67d6f2fe6d9651778b6157c98a
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-08-20 15:05:24 -0600

    updated config and .evn for deployment on Render

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,6 @@
-// Import Sequelize library
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-// Create a Sequelize instance, using DB_URL if available, otherwise use individual credentials
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(
@@ -15,5 +13,4 @@ const sequelize = process.env.DB_URL
       }
     );
 
-// Export the Sequelize instance
 module.exports = sequelize;
